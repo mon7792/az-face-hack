168e8330734379153b647759b0f8ee06e7e1a330
mon7792 <monish7792@gmail.com>
2021-04-04 15:11:13 +0100

    added demo post file.

--- faceapp/lib/utils/post.dart
+++ faceapp/lib/utils/post.dart
@@ -9,5 +9,43 @@ class Utils {
     );
 
     return response;
+
+    // var request = new http.MultipartRequest("POST", url);
+    // request.fields['uuid'] = 'nweiz@google.com';
+    // request.files.add(new http.MultipartFile.fromBytes('file', faceBytes));
+    // var response = await request.send();
+
+    // response.
+    // // request.send().then((response) {
+    // //   if (response.statusCode == 200) print("Uploaded!");
+    // // });
+
+    // return response;
   }
 }
+
+// http.Response response = await http.post(
+//   url,
+//   headers: ,
+//   body: faceBytes,
+// );
+
+// var request = http.MultipartRequest('POST', url);
+// request.files.add(http.MultipartFile('file', faceBytes, faceBytes));
+
+// var response = await request.send();
+
+// class Utils {
+//   Future<http.Response> registerFace(dynamic faceBytes) async {
+//     var url = Uri.parse("https://az-face-hack.azurewebsites.net/verify");
+
+//     Map<String, String> header = {
+//       "Content-Type": "multipart/form-data",
+//     };
+
+//     http.Response response =
+//         await http.post(url, headers: header, body: faceBytes);
+
+//     return response;
+//   }
+// }
